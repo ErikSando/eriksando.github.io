b88c78369c233004e201dc0c666ea37f85dbe7da
Erik <erik19sando@gmail.com>
2022-02-21 07:21:47 +0930

    idk
    beanis

--- script.js
+++
@@ -1,7 +0,0 @@
-let projectDivs = document.getElementsByTagName('project-div');
-
-for (let i = 0; i < projectDivs.length; i++) {
-    projectDivs[i].onclick = function () {
-        console.log('cleck')
-    }
-}
