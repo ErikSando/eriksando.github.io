3d9b93c52768222e784a6ff372cdd7ed7b155e1e
Erik <erik19sando@gmail.com>
2022-04-29 19:43:24 +0930

    Fixed stopwatch
    The stopwatch now updates correctly and is accurate.

--- projects/platfomer/project/js/stopwatch.js
+++ projects/platfomer/project/js/stopwatch.js
@@ -1,61 +1,64 @@
 class Stopwatch {
+    #running = false;
+    
     constructor() {
         this.millisecs = 0;
         this.seconds = 0;
         this.minutes = 0;
         this.running = false;
         this.wait = 10;
+        this.lastUpdate = Date.now();
+        this.updateTime(Date.now());
     }
 
     start() {
-        this.running = true;
-
-        this.updateTime();
+        this.#running = true;
     }
 
     stop() {
-        this.running = false;
+        this.#running = false;
     }
 
-    updateTime(time) {
-        const dt = (time - this.lastUpdate) / 100;
+    updateTime = (time) => {
+        const dt = (time - this.lastUpdate);
         this.lastUpdate = time;
-
-        if (!this.running) return;
         
+        if (!this.#running || !dt || dt < 0 || dt > 1000) return requestAnimationFrame(this.updateTime);
+
         this.millisecs += dt;
 
         // If a second has past
-        if (this.millisecs >= 100) {
-            this.millisecs = 0;
-            this.seconds += 1;
+        if (this.millisecs >= 1000) {
+            this.millisecs -= 1000;
+            this.seconds++;
         }
 
         // If a minute has passed
         if (this.seconds >= 60) {
-            this.seconds = 0;
-            this.minutes += 1;
+            this.seconds -= 60;
+            this.minutes++;
         }
 
         requestAnimationFrame(this.updateTime);
     }
 
     getTime() {
-        let _millisecs = this.millisecs;
+        let _millisecs = Math.floor(this.millisecs);
         let _seconds = this.seconds;
         let _minutes = this.minutes;
 
-        // Add a zero if less than ten (i.e. 7 becomes 07)
         if (_millisecs < 10) {
-            _millisecs = ('0' + _millisecs).slice(-2);
+            _millisecs = '00' + _millisecs;
+        } else if (_millisecs < 100) {
+            _millisecs = '0' + _millisecs;
         }
 
         if (_seconds < 10) {
-            _seconds = ('0' + _seconds).slice(-2);
+            _seconds = '0' + _seconds;
         }
 
         if (_minutes < 10) {
-            _minutes = ('0' + _minutes).slice(-2);
+            _minutes = '0' + _minutes;
         }
 
         return _minutes + ':' + _seconds + ':' + _millisecs;
