3a4a072160979cc7135563fd7390206fdc5247c5
Erik <erik19sando@gmail.com>
2022-05-29 20:21:03 +0930

    Added more player animations
    Added more player animations to the game I'm working on with my game engine.

--- projects/game-engine/Projects/game/Assets/Scripts/Player.js
+++ projects/game-engine/Projects/game/Assets/Scripts/Player.js
@@ -3,6 +3,27 @@ let rIdle2 = new Image();
 rIdle1.src = 'Assets/Textures/right idle 1.png';
 rIdle2.src = 'Assets/Textures/right idle 2.png';
 
+let lIdle1 = new Image();
+let lIdle2 = new Image();
+lIdle1.src = 'Assets/Textures/left idle 1.png';
+lIdle2.src = 'Assets/Textures/left idle 2.png';
+
+let rJump = new Image();
+rJump.src = 'Assets/Textures/right jump.png';
+
+let lJump = new Image();
+lJump.src = 'Assets/Textures/left jump.png';
+
+let rFall1 = new Image();
+let rFall2 = new Image();
+rFall1.src = 'Assets/Textures/right fall 1.png';
+rFall2.src = 'Assets/Textures/right fall 2.png';
+
+let lFall1 = new Image();
+let lFall2 = new Image();
+lFall1.src = 'Assets/Textures/left fall 1.png';
+lFall2.src = 'Assets/Textures/left fall 2.png';
+
 let rRun1 = new Image();
 let rRun2 = new Image();
 let rRun3 = new Image();
@@ -13,6 +34,17 @@ rRun2.src = 'Assets/Textures/right run 2.png';
 rRun3.src = 'Assets/Textures/right run 3.png';
 rRun4.src = 'Assets/Textures/right run 4.png';
 rRun5.src = 'Assets/Textures/right run 5.png';
+
+let lRun1 = new Image();
+let lRun2 = new Image();
+let lRun3 = new Image();
+let lRun4 = new Image();
+let lRun5 = new Image();
+lRun1.src = 'Assets/Textures/left run 1.png';
+lRun2.src = 'Assets/Textures/left run 2.png';
+lRun3.src = 'Assets/Textures/left run 3.png';
+lRun4.src = 'Assets/Textures/left run 4.png';
+lRun5.src = 'Assets/Textures/left run 5.png';
 
 const Player = new class extends UpdatesEachFrame {
     grounded = false;
@@ -27,22 +59,26 @@ const Player = new class extends UpdatesEachFrame {
         this.go = new GameObject(Vector(300, 0), Vector(48, 112));
         this.anims = {
             left: {
-                idle: new _Animation(),
-                run: new _Animation(),
-                jump: new _Animation(),
-                fall: new _Animation()
+                idle: new _Animation([lIdle1, lIdle2]),
+                run: new _Animation([lRun1, lRun2, lRun3, lRun4, lRun5]),
+                jump: new _Animation([lJump]),
+                fall: new _Animation([lFall1, lFall2])
             },
 
             right: {
                 idle: new _Animation([rIdle1, rIdle2]),
                 run: new _Animation([rRun1, rRun2, rRun3, rRun4, rRun5]),
-                jump: new _Animation(),
-                fall: new _Animation()
+                jump: new _Animation([rJump]),
+                fall: new _Animation([rFall1, rFall2])
             }
         }
 
+        this.anims.left.idle.fps = 2;
         this.anims.right.idle.fps = 2;
+        this.anims.left.run.fps = 6;
         this.anims.right.run.fps = 6;
+        this.anims.left.fall.fps = 2;
+        this.anims.right.fall.fps = 2;
 
         this.anim = this.anims[this.direction].idle;
     }
@@ -53,8 +89,13 @@ const Player = new class extends UpdatesEachFrame {
 
         this.vel.x = hMovement * this.speed;
 
-        if (this.vel.x) this.anim = this.anims[this.direction].run;
-        else this.anim = this.anims[this.direction].idle;
+        if (this.vel.x) {
+            if (this.vel.x < 0) this.direction = 'left';
+            else if(this.vel.x > 0) this.direction = 'right';
+
+            this.anim = this.anims[this.direction].run;
+        
+        } else this.anim = this.anims[this.direction].idle;
 
         if ((vMovement == 1 || Input.GetKey('Space')) && this.grounded) this.vel.y = -this.jumpPower;
 
@@ -71,8 +112,8 @@ const Player = new class extends UpdatesEachFrame {
         }
 
         if (!this.grounded) {
-            if (this.vel.y > 0) {} // jump anim
-            else {} // fall anim
+            if (this.vel.y > 0) this.anim = this.anims[this.direction].fall;
+            else this.anim = this.anims[this.direction].jump;
         }
 
         if (!this.anim.playing) this.anim.Play();
