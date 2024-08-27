40af5f21aaa627fbdf14928be841dd75dbe854d3
ErikSando <99470600+ErikSando@users.noreply.github.com>
2024-08-27 22:41:32 +0930

    Changed variable names
    very cool epic is better than very epic cool so i changed it

--- projects/matrix-stuff/js/main.js
+++ projects/matrix-stuff/js/main.js
@@ -5,11 +5,11 @@ let verycoolepickey = [
     [3, 7]
 ]
 
-let veryepiccoolmessage = "Hello, this message includes punctuation! Question mark? Numbers: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10. Some smybols: !@#$%^&*()-=_+[]{}</>.\\\"';~`|";
+let verycoolepicmessage = "Hello, this message includes punctuation! Question mark? Numbers: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10. Some smybols: !@#$%^&*()-=_+[]{}</>.\\\"';~`|";
 
-let veryepiccoolencodedmessage = EncodeMessage2(veryepiccoolmessage, verycoolepickey);
-let veryepiccooldecodedmessage = DecodeMessage2(veryepiccoolencodedmessage, verycoolepickey);
+let verycoolepicencodedmessage = EncodeMessage2(verycoolepicmessage, verycoolepickey);
+let verycoolepicdecodedmessage = DecodeMessage2(verycoolepicencodedmessage, verycoolepickey);
 
-console.log(FormatMessage(veryepiccoolencodedmessage));
+console.log(FormatMessage(verycoolepicencodedmessage));
 
-console.log(veryepiccoolencodedmessage, veryepiccooldecodedmessage);
+console.log(verycoolepicencodedmessage, verycoolepicdecodedmessage);
