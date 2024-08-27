e0667b4731c7473854d373293f96057138fdf163
ErikSando <99470600+ErikSando@users.noreply.github.com>
2024-08-27 22:39:31 +0930

    Changed key variable name
    Same reason as last time, the user may try use it and I don't want to give them an error

--- projects/matrix-stuff/js/main.js
+++ projects/matrix-stuff/js/main.js
@@ -1,14 +1,14 @@
 // this file was intended to handle GUI and stuff, but I havent started with making that yet
 
-let key = [
+let verycoolepickey = [
     [4, 9],
     [3, 7]
 ]
 
 let veryepiccoolmessage = "Hello, this message includes punctuation! Question mark? Numbers: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10. Some smybols: !@#$%^&*()-=_+[]{}</>.\\\"';~`|";
 
-let veryepiccoolencodedmessage = EncodeMessage2(veryepiccoolmessage, key);
-let veryepiccooldecodedmessage = DecodeMessage2(veryepiccoolencodedmessage, key);
+let veryepiccoolencodedmessage = EncodeMessage2(veryepiccoolmessage, verycoolepickey);
+let veryepiccooldecodedmessage = DecodeMessage2(veryepiccoolencodedmessage, verycoolepickey);
 
 console.log(FormatMessage(veryepiccoolencodedmessage));
 
